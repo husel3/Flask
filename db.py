3fd08668f8a774163f7e7cbb0e74e608bfe0ef3b
Enkhkhuslen <vh857977@gmail.com>
2025-04-13 15:40:52 +0800

    basics

---
+++ db.py
@@ -0,0 +1,7 @@
+stores = {"1": {"id": "1", "name": "Ikea"}, "2": {"id": "2", "name": "BSB"}}
+
+items = {
+    "1": {"id": "1", "store_id": "1", "name": "Chair", "price": 17.99},
+    "2": {"id": "2", "store_id": "1", "name": "Table", "price": 180.50},
+    "3": {"id": "3", "store_id": "2", "name": "Sofa", "price": 380.50},
+}
